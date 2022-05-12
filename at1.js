f18ef107c4928527bff4865f11d2eba99e0147d1
gutierresleh <102757734+gutierresleh@users.noreply.github.com>
2022-05-12 15:41:22 -0300

    Add files via upload

---
+++ at1.js
@@ -0,0 +1,7 @@
+let idade = prompt ('Coloque seu ano de nascimento');
+
+if(idade <= 2006){
+    alert(`Você pode votar`); 
+} else{
+    alert(`Você não pode votar`);
+}
---
+++ at2.js
@@ -0,0 +1,7 @@
+let senha = prompt ('Qual é a senha?');
+
+if(senha == 1234){
+    alert('ACESSO PERMITIDO')
+}else{
+    alert('ACESSO NEGADO')
+}
